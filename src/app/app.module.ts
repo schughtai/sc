02058f676af482fcd223d0077c94103cff02e4de
Salik Chughtai <Salik456@gmail.com>
2018-03-24 14:38:51 +0800

    Added Routes + empty chat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { ChatFormComponent } from './chat-form/chat-form.component';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,18 +8,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChatappComponent } from './chatapp/chatapp.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    ChatappComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'', component:HomeComponent},
+      {path:'chat', component: ChatappComponent},
+      {path:'shurl', component: SignupFormComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
